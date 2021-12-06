ffe056efef550a23193688067f42e9fa8435bfc9
CristianMellado <85757675+CristianMellado@users.noreply.github.com>
2021-12-06 07:07:51 -0500

    Update Types_house.h

--- Types_house.h
+++ Types_house.h
@@ -1,4 +1,4 @@
-/****************************************************************
+/***************************************************************
 Proyecto final
 
 Departamento de Ciencia de la Computación
@@ -8,7 +8,7 @@ Alumnos:
  - Cristian Mellado Baca
  - Marcelo Torres Acuña
  - Alexander Carpio Mamani
-*****************************************************************/
+****************************************************************/
 
 #pragma once
 #define _GLIBCXX_USE_CXX11_ABI 0
